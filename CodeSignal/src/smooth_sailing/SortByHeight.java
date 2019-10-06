d403f63259363ded0beca8f8194bae6b4df41f38
Chamle <chamle1602@gmail.com>
2019-10-06 23:13:20 +0900

    Create SortByHeight.java

---
+++ CodeSignal/src/smooth_sailing/SortByHeight.java
@@ -0,0 +1,30 @@
+package smooth_sailing;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortByHeight {
+
+	static int[] sortByHeight(int[] a) {
+		ArrayList<Integer> temp = new ArrayList<>();
+		for(int i = 0;i<a.length;i++){
+			if(a[i]!=-1){
+				temp.add(a[i]);
+			}
+		}
+		Collections.sort(temp);
+		int d=0;
+		for(int i = 0;i<a.length;i++){
+			if(a[i]!=-1){
+				a[i] = temp.get(d);
+				d++;
+			}
+		}
+		return a;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
